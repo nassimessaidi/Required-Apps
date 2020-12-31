52e4bb38f2437aa559e632dc5ae33b34beac8082
Nassim Essaidi <Contact@essaidi.me>
2020-12-31 02:11:01 +0100

    add logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,9 +6,14 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="nav-bar mt-4 mb-2">
-        <h1 className="heading1">NASSIM ESSAIDI</h1>
+        <img
+          className="mb-3"
+          src="https://4.bp.blogspot.com/-i2NtKpGy0yU/X4ol9mCxRjI/AAAAAAAACic/ba4Nj3Q7gDgdjgsmHacXaJHIpsH9bQJEwCK4BGAYYCw/s1600/blog%2Blogo.png"
+          alt="NASSIM ESSAIDI"
+        />
         <p className="text-muted">
-          Essential apps and software to me when I install a fresh windows 10
+          Essential apps and software to me when I will install a fresh windows
+          10
         </p>
       </nav>
     );

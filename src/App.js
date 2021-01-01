8cc05fed78cc19750b1c7f67323e125e0b6eaa80
Nassim Essaidi <Contact@essaidi.me>
2021-01-01 02:24:54 +0100

    add 404 page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Cards from "./components/cards";
 import Navbar from "./components/navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SubCard from "./components/subCard";
+import NotFound from "./components/404";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={Cards} />
           <Route path="/python" exact component={SubCard} />
+          <Route path="/" render={() => <NotFound/>} />
         </Switch>
       </div>
     </Router>
---
+++ src/components/404.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./styles/404.css";
+
+class NotFound extends Component {
+  state = {};
+  render() {
+    return (
+      <section className="section">
+        <h1>404 Not Found</h1>
+      </section>
+    );
+  }
+}
+
+export default NotFound;
